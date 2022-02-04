36dc5f863b249105cf2d696c548d6111dce88c89
Moshe Reubinoff <moshe@ownbackup.com>
2022-02-04 22:09:07 +0200

    test

--- flutter_client/lib/main.dart
+++ flutter_client/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Welcome to Flutter', home: Moshe());
+    return const MaterialApp(title: 'Welcome to Flutter', home: Moshe());
   }
 }
 
