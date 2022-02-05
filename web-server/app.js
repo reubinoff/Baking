2e278fa2249491f25382d36dc2505ac8eb51b634
Moshe Reubinoff <moshe@ownbackup.com>
2022-02-05 13:05:40 +0200

    adding chart for web

--- web-server/app.js
+++ web-server/app.js
@@ -13,7 +13,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.get('/healthcheck', (req, res) =>
+    res.send('OK')
+);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
