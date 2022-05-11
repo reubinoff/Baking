63c20e0302eb6dd9e31bbda3fffad6a8a642f795
Moshe Reubinoff <moshe@ownbackup.com>
2022-05-11 15:47:16 +0800

    test

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -12,6 +12,7 @@ environ["DB_HOST"] = "127.0.0.1"
 environ["DB_NAME"] = "baking-db-test-" + str(random.random())
 environ["DB_USER"] = "postgres"
 environ["azure_storage_connection_string"] = "test.com"
+environ["is_debug"] = "True"
 
 from baking import config
 from baking.database.core import engine, get_sql_url
