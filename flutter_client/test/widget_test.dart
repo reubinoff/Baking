6b37a9ebe8c493ac2e5bc0db8ba012043a01fd28
Moshe Reubinoff <moshe@ownbackup.com>
2022-05-28 16:17:36 +0300

    test

--- flutter_client/test/widget_test.dart
+++ flutter_client/test/widget_test.dart
@@ -1,17 +1,6 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-// import 'package:flutter/material.dart';
-// import 'package:baking_client/components/recipe_card.dart';
 import 'package:baking_client/models/recipe.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-// import 'package:baking_client/main.dart';
 
 void main() {
   testWidgets('Baking app test', (WidgetTester tester) async {
@@ -25,16 +14,5 @@ void main() {
       cdnUrl: '',
     );
     debugPrint(r.toString());
-    // // Verify that our counter starts at 0.
-    // expect(find.text('0'), findsOneWidget);
-    // expect(find.text('1'), findsNothing);
-
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
-
-    // // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
   });
 }
