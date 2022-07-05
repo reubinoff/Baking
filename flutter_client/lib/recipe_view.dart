44e1fe99c2fc3d2a89d40fad3c9d3b9b4fab54d5
Moshe Reubinoff <moshe@ownbackup.com>
2022-07-05 18:31:41 +0300

    remove debugging

--- flutter_client/lib/recipe_view.dart
+++ flutter_client/lib/recipe_view.dart
@@ -50,10 +50,8 @@ class _RecipeViewState extends State<RecipeView> {
                   },
                   child: NotificationListener<ScrollNotification>(
                       onNotification: (ScrollNotification scrollInfo) {
-                        debugPrint("AA");
                         if (scrollInfo is ScrollEndNotification &&
                             scrollInfo.metrics.extentAfter == 0) {
-                          debugPrint("BB");
                           notifier.getMore();
                           return true;
                         }
