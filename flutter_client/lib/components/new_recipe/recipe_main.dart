138233d4579ac12582cdcf7885fd4334b8856f2c
Moshe Reubinoff <moshe@ownbackup.com>
2022-10-03 10:09:28 +0300

    change CI

--- flutter_client/lib/components/new_recipe/recipe_main.dart
+++ flutter_client/lib/components/new_recipe/recipe_main.dart
@@ -1,4 +1,3 @@
-import 'package:baking_client/components/new_recipe/new_procedure.dart';
 import 'package:flutter/material.dart';
 
 class RecipeMainForm extends StatefulWidget {
@@ -21,57 +20,31 @@ class RecipeMainFormState extends State<RecipeMainForm> {
       appBar: AppBar(
         title: const Text('New Recipe'),
       ),
-      body: _buildForm(),
-    );
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Enter your recipe name',
-                ),
-                autofocus: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'We must have it';
-                  }
-                  return null;
-                },
-              )),
-          Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child: TextFormField(
-                maxLines: 3,
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Enter your recipe description',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Give us Some information';
-                  }
-                  return null;
-                },
-              )),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // Open NewProcedure Form
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const NewProcedure()),
-                );
+      body: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Enter recipe name',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
               },
-              child: const Text('Submit'),
             ),
-          ),
-        ],
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: ElevatedButton(
+                onPressed: null,
+                child: Text('Next'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
