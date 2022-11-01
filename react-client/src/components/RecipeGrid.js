04280f4bf963b6d105cf12b8cac9f1b049033b15
Moshe Reubinoff <moshe@ownbackup.com>
2022-11-01 21:03:12 +0200

    test

--- react-client/src/components/RecipeGrid.js
+++ react-client/src/components/RecipeGrid.js
@@ -3,35 +3,49 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import RecipeCard from "./RecipeCard";
 import PropTypes from "prop-types";
-
+import Spinner from "react-bootstrap/Spinner";
 
 class RecipeGrid extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       recipes: this.props.recipes,
+      loading: true,
     };
   }
 
+  // featch data
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch("https://service.baking.reubinoff.com/recipe?page=2&itemsPerPage=5")
+      .then((response) => response.json())
+      .then((data) => this.setState({ recipes: data.items, loading: false }));
+  }
+
   render() {
-    const { recipes, loading } = this.state;
+    const { loading, recipes } = this.state;
 
     if (loading) {
-      return <div>Loading...</div>;
-    }
-    
-    return (
-      <Row xs={1} md={4} className="g-4">
-        {recipes.map((recipe) => (
-        <Col key={recipe.id}>
-          <RecipeCard recipe={recipe} />
-        
-        </Col>
-        ))}
-      </Row>
+      return (
+        <div className="d-flex align-items-center">
+          <Spinner animation="border" role="status" size="xxl" className="mr-2">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      );
+    } else {
+      return (
+        <Row xs={1} md={4} className="g-4">
+          {recipes.map((recipe) => (
+            <Col key={recipe.id}>
+              <RecipeCard recipe={recipe} />
+            </Col>
+          ))}
+        </Row>
       );
     }
   }
+}
 
 RecipeGrid.propTypes = {
   recipes: PropTypes.array.isRequired,
