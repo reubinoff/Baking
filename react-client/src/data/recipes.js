1c7349df52e239cfd615242b6be714c36fcbd793
Moshe Reubinoff <moshe@ownbackup.com>
2022-12-06 13:25:16 +0200

    items

--- react-client/src/data/recipes.js
+++ react-client/src/data/recipes.js
@@ -3,7 +3,7 @@ import handleError from "../utils/handleError";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-export function useRecipes(itemsPerPage = 3) {
+export function useRecipes(itemsPerPage = 10) {
   const queryMethod = async ({ pageParam = 1 }) => {
     const path = `${BASE_URL}/recipe?page=${pageParam}&itemsPerPage=${itemsPerPage}`;
 
