588736fb7b4617cac86592e689032c36ee78ce4b
Moshe Reubinoff <moshe@ownbackup.com>
2022-12-08 21:28:50 +0200

    tet

--- server/src/baking/logger.py
+++ server/src/baking/logger.py
@@ -4,39 +4,8 @@ import sys
 from baking.config import settings
 import logging.config
 
-LOGGING =  {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'logzioFormat': {
-            'format': '{"additional_field": "value"}',
-            'validate': False
-        }
-    },
-    'handlers': {
-        'logzio': {
-            'class': 'logzio.handler.LogzioHandler',
-            'level': "INFO",
-            'formatter': 'logzioFormat',
-            'token': settings.logzio_token,
-            'logzio_type': 'python',
-            'logs_drain_timeout': 5,
-            'url': 'https://listener-uk.logz.io:8071',
-        }
-    },
-    'loggers': {
-        '': {
-            'level': settings.log_level,
-            'handlers': ['logzio'],
-            'propagate': True
-        }
-    }
-}
 
 def init_logger():
-    if settings.logzio_token:
-        logging.config.dictConfig(LOGGING)
-
     handler = logging.StreamHandler(stream=sys.stdout)
 
     logger = logging.getLogger()
