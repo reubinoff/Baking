8522cc0fe68ec16b45593e44418388543df89c13
Moshe Reubinoff <moshe@ownbackup.com>
2022-12-09 00:18:35 +0200

    split components

--- react-client/src/components/BakingNavBar.jsx
+++ react-client/src/components/BakingNavBar.jsx
@@ -1,81 +1,18 @@
 import React from "react";
-import { styled, alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
 import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
-import useScrollTrigger from "@mui/material/useScrollTrigger";
-import Slide from "@mui/material/Slide";
-import PropTypes from "prop-types";
+
 import Box from "@mui/material/Box";
 import Tooltip from "@mui/material/Tooltip";
 import Avatar from "@mui/material/Avatar";
 import BakingDrawer from "./BakingDrawer";
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import BakingSearchBar from "./BakingSearchBar";
+import HideOnScroll from "./HideOnScroll";
 
-function HideOnScroll(props) {
-  const { children, window } = props;
-
-  const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
-  });
-
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  );
-}
-
-HideOnScroll.propTypes = {
-  children: PropTypes.element.isRequired,
-  window: PropTypes.func,
-};
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
 
 export default function SearchAppBar(props) {
     const [, setAnchorElUser] = React.useState(null);
@@ -122,15 +59,8 @@ export default function SearchAppBar(props) {
           >
             Baking
           </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search>
+          <BakingSearchBar></BakingSearchBar>
+
           <Box sx={{ flexGrow: 0, ml: 2 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
@@ -139,7 +69,12 @@ export default function SearchAppBar(props) {
             </Tooltip>
           </Box>
         </Toolbar>
-        <SwipeableDrawer anchor="left" open={open} onClose={toggleDrawer(false)}>
+        <SwipeableDrawer
+          anchor="left"
+          open={open}
+          onOpen={toggleDrawer(true)}
+          onClose={toggleDrawer(false)}
+        >
           <BakingDrawer setOpen={toggleDrawer(false)}></BakingDrawer>
         </SwipeableDrawer>
       </AppBar>
