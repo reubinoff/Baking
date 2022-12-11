4f349f2449841430e1debad219e79e455405dea8
Moshe Reubinoff <moshe@ownbackup.com>
2022-12-11 20:18:08 +0200

    estsetst

--- react-client/src/pages/Recipe.jsx
+++ react-client/src/pages/Recipe.jsx
@@ -3,6 +3,16 @@ import { Link, useLocation } from "react-router-dom";
 
 function Recipe() {
 const { state } = useLocation();
+    if (state?.recipe === undefined) {
+        return (
+        <div>
+            <h2>Recipe not found</h2>
+            <p>
+            <Link to="/">Go to the home page</Link>
+            </p>
+        </div>
+        );
+    }
   return (
     <div>
       <h2>{state.recipe.name}</h2>
