fb44c85047d252715edd7d2859151d6aa5261b14
Moshe Reubinoff <moshe@ownbackup.com>
2022-12-14 12:20:34 +0200

    test

--- react-client/src/components/BakingSearchBar.jsx
+++ react-client/src/components/BakingSearchBar.jsx
@@ -102,13 +102,12 @@ export default function BakingSearchBar(props) {
 
 
 const top100Films = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-]
+  { title: "fros", year: 1994 },
+  { title: "track", year: 1972 },
+  { title: "nokia", year: 1974 },
+  { title: "no result option", year: 1974 },
+
+];
 
 BakingSearchBar.propTypes = {
   show: PropTypes.bool
