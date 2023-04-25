f2bf8de0996c896778ae9f5fc50d92dfbd7d66ae
Moshe Reubinoff <moshe@ownbackup.com>
2023-04-25 21:35:43 +0300

    test

--- server/scripts/prestart.sh
+++ server/scripts/prestart.sh
@@ -2,17 +2,17 @@
 
 
 #DB 
-echo "hello $DB_HOST"
-while ! pg_isready -h $DB_HOST -U $DB_USER
-do
-    echo "$(date) - waiting for database to start"
-    sleep 10
-done
-echo "Running migration..."
+# echo "hello $DB_HOST"
+# while ! pg_isready -h $DB_HOST -U $DB_USER
+# do
+#     echo "$(date) - waiting for database to start"
+#     sleep 10
+# done
+# echo "Running migration..."
 
 
-cd ./baking
-alembic upgrade head
+# cd ./baking
+# alembic upgrade head
 
 
 echo "starting!!"
