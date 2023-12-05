78b74490a7e2cb544f3f65a52ccd949bf16b2abc
Moshe Reubinoff <moshe@ownbackup.com>
2023-12-05 13:49:25 +0200

    te

---
+++ react-client/.storybook/preview.js
@@ -0,0 +1,20 @@
+/** @type { import('@storybook/react').Preview } */
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/material-icons";
+
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ react-client/src/storiesa/FavoriteButton.stories.jsx
@@ -0,0 +1,11 @@
+import { FavoriteButton } from '../components/recipeComponents/FavoriteRecipeButton';
+
+export default {
+    component: FavoriteButton,
+};
+
+export const FirstStory = {
+    args: {
+        //👇 The args you need here will depend on your component
+    },
+};
